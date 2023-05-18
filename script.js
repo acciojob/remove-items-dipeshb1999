c44dd8f4dedb82233d2b8cb9e7a02819baecccd5
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-18 11:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+        function removeColor() {
+            var colorSelect = document.getElementById("colorSelect");
+            var selectedIndex = colorSelect.selectedIndex;
+            if (selectedIndex !== -1) {
+                colorSelect.remove(selectedIndex);
+            }
+        }
